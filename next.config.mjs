66d7152102955cdb3dae437f62139026609ddf8f
Mateosanzep <mateosanzep@gmail.com>
2025-12-02 08:24:49 -0600

    Render corrects

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  env: {
+    NEXT_PUBLIC_EXPRESS_API_URL: process.env.NEXT_PUBLIC_EXPRESS_API_URL || 'http://localhost:3001',
+  },
 };
 
 export default nextConfig;
